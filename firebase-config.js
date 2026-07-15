87ee6923c4980e357bc3596c3649e1b7174fd376
asko-serwis <a.skowron8@wp.pl>
2026-07-15 13:35:02 +0200

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.ASKO_FB = {
+  apiKey: "AIzaSyC5xmMPwqPseQJhCEwMWQp9QHHtWWnPZQo",
+  authDomain: "asko-zlecenia.firebaseapp.com",
+  projectId: "asko-zlecenia",
+  storageBucket: "asko-zlecenia.firebasestorage.app",
+  messagingSenderId: "70403683496",
+  appId: "1:70403683496:web:fd4bbf0107ac15d70cbe9d"
+};
